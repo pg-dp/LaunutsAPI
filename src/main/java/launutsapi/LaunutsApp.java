2b2501054213b7557003939276ea849028c5bcdf
Gourab Sahu <gourab@mail.uni-paderborn.de>
2020-05-16 14:56:58 +0200

    Push LaunutsAPI project to github

---
+++ src/main/java/launutsapi/LaunutsApp.java
@@ -0,0 +1,13 @@
+package launutsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaunutsApp {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(LaunutsApp.class, args);
+		}
+
+}
